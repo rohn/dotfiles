0e479abd92bcd6480b3d4badb8bf4c268c1836cd
Rohn Blake <rblake@Rohn-Blakes-MacBook-Pro.local>
2013-05-19 15:25:48 -0500

    fixup some

---
+++ homebrew/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  x You should probably install Homebrew first:"
+  echo "    https://github.com/mxcl/homebrew/wiki/installation"
+  exit
+fi
+
+# Install homebrew packages
+brew install mifix/grc/grc coreutils spark
+
+exit 0
